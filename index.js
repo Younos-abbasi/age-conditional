9518241c69a6fc623c0ade240f22f2a6d597624f
kaan <abbasikaan@gmail.com>
2024-03-28 11:04:11 +0100

    project completed

--- index.js
+++ index.js
@@ -10,5 +10,13 @@ mySubmit.onclick = () => {
 
   if (age >= 100) {
     result.textContent = `You are too old to enter this site!`;
+  } else if (age < 0) {
+    result.textContent = `You are not born Yet!`;
+  } else if (age > 18 && age < 100) {
+    result.textContent = `You are old enough to enter this site!`;
+  } else if (age < 18 || age >= 0) {
+    result.textContent = `You are not allowed!`;
+  } else {
+    result.textContent = `Please provide your exact age in number!`;
   }
 };
